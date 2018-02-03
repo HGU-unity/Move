aa7f938dde91ee7d452307ca3f3119ab1a23fda7
Geoyeob Kim <gasbank@msn.com>
2018-02-03 15:41:23 +0900

    키보드 방향키로 이동하기

---
+++ Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class Move : MonoBehaviour {
+    void Start () {
+        Debug.Log("Start() called.");
+	}
+	void Update () {
+        var dx = Input.GetAxis("Horizontal");
+        var dz = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(dx, 0, dz));
+	}
+    private void OnTriggerEnter(Collider other) {
+        Debug.LogFormat("OnTriggerEnter called. other is {0}", other.name);
+    }
+    private void OnTriggerExit(Collider other) {
+        Debug.LogFormat("OnTriggerExit called. other is {0}", other.name);
+    }
+}
